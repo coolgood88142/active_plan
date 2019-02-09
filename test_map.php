de723d0f2df1d1b030186500a8854e44c5a69e1d
Kai <coolgood88142@gmail.com>
2019-02-10 03:09:43 +0800

    02/09

--- test_map.php
+++ test_map.php
@@ -4,10 +4,14 @@
 	<meta charset="UTF-8">
   <meta name="author" content="oxxo.studio">
   <meta name="copyright" content="oxxo.studio">
-	<title>Google Maps API - 網頁載入地圖 ( 起手式 ) demo 01 - OXXO.STUDIO</title>
+  <title>地址</title>
+  <?php include("link.php");?>
 </head>
+<?php session_start();
+  
+?>
 <body>
-    <iframe width="400" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
+  <iframe width="400" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
     src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBXjRJwCEvqKgxCnUsI-kGALYnJx0InesE
     &q=動物園" allowfullscreen></iframe>
 </body>
